11a4fe5302877e6e422ce927d4ec86a8875ad1d7
Gregory Schare <grschare1315@gmail.com>
2023-05-03 22:04:50 -0400

    add more Ast nodes

--- src/Ast.hs
+++ src/Ast.hs
@@ -11,17 +11,31 @@ type TermName = Unbound.Name Term
 type Type = Term
 
 data Term
-  = U                                      -- the type of types `U`
-  | Var TermName                           -- variables `x`
-  | Lam (Unbound.Bind TermName Term)       -- abstraction `λx. a`
-  | App Term Term                          -- application `a b`
-  | Pi Type (Unbound.Bind TermName Type)   -- function type `(x : A) -> B`
-  | Ann Term Type                          -- annotated terms `(a : A)`
+  = Var TermName                            -- variables `x`
+  | Lam (Unbound.Bind TermName Term)        -- abstraction `λx. a`
+  | Pi Type (Unbound.Bind TermName Type)    -- function type `(x : A) -> B`
+  | App Term Term                           -- application `a b`
+  | Ann Term Type                           -- annotated terms `(a : A)`
+  | U                                       -- the type of types `U`
+  | UnitType                                -- Unit type
+  | UnitLit                                 -- sole inhabitant of Unit, `()`
+  | BoolType                                -- Bool type
+  | BoolLit Bool                            -- two inhabitants of Bool, True and False
+  | If Term Term Term                       -- eliminator for Bool, `p ? c | e`
+  | Sigma Type (Unbound.Bind TermName Type) -- Sigma type, `(x : A, B)`
+  | Pair Term Term                          -- constructor for Sigma types, `(a,b)`
+  | LetPair Term (Unbound.Bind (TermName, TermName) Term) -- destructor for Sigma types
+  -- TODO:
+  -- Type equality (Eq)
+  -- Type Constructors (TCon)
+  -- Term Constructors (DCon)
+  -- case analysis (Match)
+  -- Let pattern matching
   deriving (Show, Generic, Unbound.Alpha, Unbound.Subst Term)
 
 {-
-xName = Unbound.string2Name "x"
-yName = Unbound.string2Name "y"
+xName = Unbound.s2n "x"
+yName = Unbound.s2n "y"
 
 idx = Lam (Unbound.bind xName (Var xName))
 idy = Lam (Unbound.bind yName (Var yName))
@@ -40,9 +54,9 @@ data Decl
   | -- | The definition of a particular name, must
     -- already have a type declaration in scope
     Def TermName Term
-  | -- | A potentially (recursive) definition of
+--  | -- | A potentially (recursive) definition of
     -- a particular name, must be declared
-    RecDef TermName Term
+    --RecDef TermName Term
   deriving (Show, Generic, Typeable)
   deriving anyclass (Unbound.Alpha, Unbound.Subst Term)
 
